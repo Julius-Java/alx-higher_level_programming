30f3b94e62953200584b1cf078ac3993fc659174
Julius-Java <emmanueljulius9102016@gmail.com>
2022-06-30 15:52:51 -0700

    0-add.py task file created

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+
+def add_0(a, b):
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    add_0(a, b)
