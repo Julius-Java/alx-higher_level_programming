ce5079a72cccf3fafc5b96ff75734c5fa2b3bab3
Julius-Java <emmanueljulius9102016@gmail.com>
2022-06-30 16:34:53 -0700

    0-add.py task file modified

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,13 +2,11 @@
 from add_0 import add
 
 
-a = 1
-b = 2
-
-
-def add_0(a, b):
+def main():
+    a = 1
+    b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
-    add_0(a, b)
+    main()
