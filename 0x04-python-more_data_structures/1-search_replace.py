f127789b239f1dbe522cea43647381a025a0a0bf
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-06 17:07:00 -0700

    1-search_replace.py file modified

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    search_index = new_list.index(search)
-    new_list.remove(search)
-    new_list.insert(search_index, replace)
+    new_list = []
+    for item in my_list:
+        new_list.append(item) if item != search else new_list.append(replace)
     return new_list
