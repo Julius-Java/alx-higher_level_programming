9bd61260771f8cfddab6528330349d861c5f2f28
Julius-Java <emmanueljulius9102016@gmail.com>
2022-07-08 17:02:18 -0700

    lists.h header file updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -15,5 +15,8 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 size_t print_dlistint(const dlistint_t *h);
 #endif
