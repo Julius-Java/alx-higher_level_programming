3abd7900a5d4ff237ff9bc9976611c8994393a31
Julius-Java <emmanueljulius9102016@gmail.com>
2022-10-05 17:10:25 +0100

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const mySentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i;
+
+for (i = 0; i < mySentences.length; i++) {
+  console.log(mySentences[i]);
+}
