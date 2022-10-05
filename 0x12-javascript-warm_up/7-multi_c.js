8860db6d0c751cf7764dc2db728092a9d48319fc
Julius-Java <emmanueljulius9102016@gmail.com>
2022-10-05 18:07:52 +0100

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
+const argv = process.argv;
+const length = argv[2];
 
-const commandArgs = process.argv;
-
-const lenArgs = commandArgs.length;
-
-if (lenArgs <= 2) {
+if (!parseInt(argv[2])) {
   console.log('Missing number of occurrences');
 } else {
-  const numInt = parseInt(commandArgs[2]);
-  if (!isNaN(numInt)) {
-    if (numInt > 1) {
-      for (let i = 0; i < numInt; i++) {
-        console.log('C is fun');
-      }
-    } else {
-      process.exit(1);
-    }
+  for (let i = 0; i < length; i++) {
+    console.log('C is fun');
   }
 }
